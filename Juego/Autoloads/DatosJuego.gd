52d5a77869972b414575cd171420d3348514e8af
07Facundo <07facundosuares@gmail.com>
2023-11-07 09:34:31 -0300

    Pulido y Optimización.

--- Juego/Autoloads/DatosJuego.gd
+++ Juego/Autoloads/DatosJuego.gd
@@ -2,8 +2,19 @@ extends Node
 
 var player_actual:Player = null setget set_player_actual, get_player_actual
 
+
+func _ready() -> void:
+	Eventos.connect("nave_destruida",self, "_on_nave_destruida")
+
 func set_player_actual(player: Player) ->void:
 	player_actual = player
 	
 func get_player_actual() ->Player:
 	return player_actual
+
+func _on_nave_destruida(nave:NaveBase, _posicion, _explosiones) ->void:
+	if nave is Player:
+		player_actual = null
+
+
+
--- Juego/Naves/Enemigo/EnemigoBase.gd
+++ Juego/Naves/Enemigo/EnemigoBase.gd
@@ -4,7 +4,7 @@ extends NaveBase
 
 var player_objetivo:Player = null
 var dir_player: Vector2
-
+var frame_actual:int = 0
  
 func _ready() -> void:
 	player_objetivo = DatosJuego.get_player_actual()
@@ -12,7 +12,9 @@ func _ready() -> void:
 
 
 func _physics_process(_delta: float) -> void:
-	rotar_hacia_player()
+	frame_actual += 1
+	if frame_actual % 3 == 0:
+		rotar_hacia_player()
 
 func rotar_hacia_player() ->void:
 	if player_objetivo:
